ab3bd42c29eb17421a8ed0bb98d15dd572753075
proud <84202885+TheProudxD@users.noreply.github.com>
2024-09-25 14:24:52 +0300

    Added calculator logic

---
+++ WpfAndGit/Calculator/ICalculatorIntegral.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WPFIntegral
+{
+    public interface ICalculatorIntegral
+    {
+        double Calculate(double downLimit, double upLimit, int count, Func<double, double> func);
+    }
+}
